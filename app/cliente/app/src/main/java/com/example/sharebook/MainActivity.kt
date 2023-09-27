01723a6ae60f64c3c459aa52258cfc638fc12eb9
danielft2 <danielalmeida808z@gmail.com>
2023-09-27 12:22:03 -0300

    feat(cliente): add jetpack composer

--- app/cliente/app/src/main/java/com/example/sharebook/MainActivity.kt
+++ app/cliente/app/src/main/java/com/example/sharebook/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.sharebook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.sharebook.ui.theme.SharebookTheme
+import com.example.sharebook.ui.theme.gray200
+import com.example.sharebook.ui.theme.green900
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            SharebookTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+   Column(modifier = Modifier
+       .fillMaxSize()
+       .background(gray200)) {
+       Text(
+           text = "Criar nova conta.",
+           color = green900,
+           style = MaterialTheme.typography.h1
+       )
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SharebookTheme {
+        Greeting()
     }
 }
