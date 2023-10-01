4ef626c5f1e3a29afcfb76874103bef2e0b12ba6
JoseVitorNobre <josevitortinf@gmail.com>
2023-09-30 22:44:40 -0300

    feat(cliente): added \"Genero\" file
    Added the initial structure of the Genero file

---
+++ app/cliente/app/src/main/java/com/example/sharebook/ui/cadastro/Genero.kt
@@ -0,0 +1,15 @@
+package com.example.sharebook.ui.cadastro
+
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Genero() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GeneroPreview() {
+    Genero()
+}
