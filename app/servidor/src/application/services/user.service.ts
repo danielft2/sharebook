2ff8a8c1e68cbba72b1ec7933c653444531ea00d
JoseVitorNobre <josevitortinf@gmail.com>
2023-11-03 22:19:22 -0300

    fix(server): fixed user.service file
    fixed user.service identation

--- app/servidor/src/application/services/user.service.ts
+++ app/servidor/src/application/services/user.service.ts
@@ -1,61 +1,58 @@
 import {
-    ConflictException,
-    Injectable,
-    NotFoundException,
-  } from '@nestjs/common';
-  import { UserRepository } from '../../infraestructure/repositories/user.repository';
-  import { User } from '../../domain/entities/user.entity';
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserRepository } from '../../infraestructure/repositories/user.repository';
+import { User } from '../../domain/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
-  
-  @Injectable()
-  export class UserService {
-    constructor(
-      private userRepository: UserRepository,
-      private jwtService: JwtService
-    ) {}
-  
-    async create(user: User) {
-      const existUser = await this.userRepository.findByEmail(
-        user.email,
-      );
-      if (existUser) throw new ConflictException();
-      let userDataCreated = await this.userRepository.create(user);
 
-      const payload = {
-        sub: user.id,
-        email: user.email,
-        name: user.nome,
-        cep: user.cep,
-        phone: user.telefone
-      }
+@Injectable()
+export class UserService {
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
-      const userDataReturn = {
-        ...userDataCreated,
-        access_token: this.jwtService.sign(payload)
-      }
-      
-      return userDataReturn;
-    }
-  
-    async findAll() {
-      return await this.userRepository.findAll();
-    }
-  
-    async findOneById(id: string) {
-      const user = await this.userRepository.findById(id);
-      if (user) {
-        const userReturn = await this.userRepository.findById(user.id);
-  
-        return userReturn;
-      }
-  
-      throw new NotFoundException();
-    }
+  async create(user: User) {
+    const existUser = await this.userRepository.findByEmail(user.email);
+    if (existUser) throw new ConflictException();
+    const userDataCreated = await this.userRepository.create(user);
 
-    async findOneByEmail(email: string) {
-      const user = await this.userRepository.findByEmail(email);
-      if (user) return user;
-      throw new NotFoundException()
-    }
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      name: user.nome,
+      cep: user.cep,
+      phone: user.telefone,
+    };
+
+    const userDataReturn = {
+      ...userDataCreated,
+      access_token: this.jwtService.sign(payload),
+    };
+
+    return userDataReturn;
   }
-  
+
+  async findAll() {
+    return await this.userRepository.findAll();
+  }
+
+  async findOneById(id: string) {
+    const user = await this.userRepository.findById(id);
+    if (user) {
+      const userReturn = await this.userRepository.findById(user.id);
+
+      return userReturn;
+    }
+
+    throw new NotFoundException();
+  }
+
+  async findOneByEmail(email: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (user) return user;
+    throw new NotFoundException();
+  }
+}
