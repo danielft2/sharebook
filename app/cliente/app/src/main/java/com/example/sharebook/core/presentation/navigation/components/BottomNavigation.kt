88f80f98a1cf1e83033a87656cb672e5139275d3
Alexson Almeida <as3904527@gmail.com>
2023-11-20 11:56:56 -0300

    ajustando a navegacao

--- app/cliente/app/src/main/java/com/example/sharebook/core/presentation/navigation/components/BottomNavigation.kt
+++ app/cliente/app/src/main/java/com/example/sharebook/core/presentation/navigation/components/BottomNavigation.kt
@@ -81,7 +81,7 @@ fun BottomNavigation(
     }) {
         NavHost(navController = navBottomController, startDestination = Constants.HOME_ROUTE) {
             composable(PrivateRoutes.HomeScreen.route) { Home(navControllerRoot) }
-            composable(PrivateRoutes.DiscoveryScreen.route) { Discovery(navController = navController) }
+            composable(PrivateRoutes.DiscoveryScreen.route) { Discovery(navController = navControllerRoot) }
             composable(PrivateRoutes.ExchangesScreen.route) { Exchanges(navControllerRoot) }
             composable(PrivateRoutes.MapsScreen.route) { Maps() }
         }
