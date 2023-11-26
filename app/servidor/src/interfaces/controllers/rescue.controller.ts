ddfe024db2e85cf0986c11332dec3b925f719b27
JoseVitorNobre <josevitortinf@gmail.com>
2023-11-25 22:15:24 -0300

    fix(server): changed the dependency of parameters in findbook functions
    changed the dependency of parameters in findbook functions
    and added a example scope to rescue

--- app/servidor/src/interfaces/controllers/rescue.controller.ts
+++ app/servidor/src/interfaces/controllers/rescue.controller.ts
@@ -1,28 +1,53 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { RescueService } from 'src/application/services/rescue.service';
 import { Rescue } from 'src/domain/entities/rescue.entity';
 
 @Controller('rescue')
 export class RescueController {
-    constructor(private readonly rescueService: RescueService){}
+  constructor(private readonly rescueService: RescueService) {}
 
-    @Post()
-    async create(@Body() rescue: Rescue) {
-        return this.rescueService.create(rescue);
-    }
+  @Post()
+  @ApiBody({
+    schema: {
+      example: {
+        data: {
+          usuario_solicitante_id: '',
+          livro_id: '',
+          status: '',
+          livro_oferecido_id: '',
+        },
+      },
+    },
+  })
+  async create(@Body() rescue: Rescue) {
+    return this.rescueService.create(rescue);
+  }
 
-    @Put()
-    async update(@Body() rescue: Rescue) {
-        return this.rescueService.update(rescue);
-    }
+  @Put()
+  @ApiBody({
+    schema: {
+      example: {
+        data: {
+          usuario_solicitante_id: '',
+          livro_id: '',
+          status: '',
+          livro_oferecido_id: '',
+        },
+      },
+    },
+  })
+  async update(@Body() rescue: Rescue) {
+    return this.rescueService.update(rescue);
+  }
 
-    @Get('book/:id')
-    async findRescuesFromABook(@Param('id') bookId: string) {
-        return
-    }
+  @Get('book/:id')
+  async findRescuesFromABook(@Param('id') bookId: string) {
+    return;
+  }
 
-    @Get('user/:id')
-    async findRescuesFromAUser(@Param('id') userId: string) {
-        return this.rescueService.findRescuesFromAUser(userId);
-    }
+  @Get('user/:id')
+  async findRescuesFromAUser(@Param('id') userId: string) {
+    return this.rescueService.findRescuesFromAUser(userId);
+  }
 }
